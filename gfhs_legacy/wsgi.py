c829428caccdb7dca8e53ffa68216af1a21e18a1
Keaton Brady <macbookpro@Keatons-MBP.lan>
2026-04-25 11:51:59 -0600

    Initial Wagtail project setup

---
+++ gfhs_legacy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gfhs_legacy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gfhs_legacy.settings.dev")
+
+application = get_wsgi_application()
