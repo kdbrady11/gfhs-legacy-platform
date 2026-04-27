caf6d4f91d4663f133585c5107a97bf73be3eddf
Keaton Brady <macbookpro@Keatons-MBP.lan>
2026-04-26 19:51:11 -0600

    Add custom public and kiosk 404 pages

--- gfhs_legacy/urls.py
+++ gfhs_legacy/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
 from django.contrib import admin
+from django.urls import include, path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -18,26 +18,14 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
 ]
 
-
 if settings.DEBUG:
-    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+urlpatterns += [
     path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "gfhs_legacy.views.custom_404"
